c2e37b48a915cb3f39adbd0e88499aa9fed12657
Vikas Dangi <mvikasdangi@gmail.com>
2025-04-14 02:12:08 +0530

    feat: initialized inventory microservice

---
+++ nexgen-server/inventory-service/inventory-service/src/test/java/com/nexgen/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexgen.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
