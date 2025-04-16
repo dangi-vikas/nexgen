0364fa5005d3b2b25b29bed13b5e42b0b8f4d8fc
Vikas Dangi <mvikasdangi@gmail.com>
2025-04-16 23:48:35 +0530

    feat: initialized Product microservice

---
+++ nexgen-server/product-service/product-service/src/test/java/com/nexgen/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexgen.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
