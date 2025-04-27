5d5779cc07bdbbad3b302859bec585a677d30cec
Vikas Dangi <mvikasdangi@gmail.com>
2025-04-28 00:46:57 +0530

    feat: added swagger documentation

--- nexgen-server/order-service/order-service/src/main/java/com/nexgen/order_service/controller/OrderController.java
+++ nexgen-server/order-service/order-service/src/main/java/com/nexgen/order_service/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.nexgen.order_service.controller;
 import com.nexgen.order_service.dto.OrderRequest;
 import com.nexgen.order_service.dto.OrderResponse;
 import com.nexgen.order_service.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,29 +14,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
+@Tag(name = "Order Controller", description = "Manage customer orders")
 @RequiredArgsConstructor
 public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "Create a new order")
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(@Valid @RequestBody OrderRequest orderRequest) {
         OrderResponse orderResponse = orderService.createOrder(orderRequest);
         return ResponseEntity.ok(orderResponse);
     }
 
+    @Operation(summary = "Get order by Order Number")
     @GetMapping("/{orderNumber}")
     public ResponseEntity<OrderResponse> getOrderByOrderNumber(@PathVariable String orderNumber) {
         OrderResponse orderResponse = orderService.getOrderById(orderNumber);
         return ResponseEntity.ok(orderResponse);
     }
 
+    @Operation(summary = "Get all orders by User ID")
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<OrderResponse>> getOrdersByUserId(@PathVariable String userId) {
         List<OrderResponse> orders = orderService.getOrdersByUserId(userId);
         return ResponseEntity.ok(orders);
     }
 
+    @Operation(summary = "Cancel an order by Order Number")
     @PutMapping("/{orderNumber}/cancel")
     public ResponseEntity<Void> cancelOrder(@PathVariable String orderNumber) {
         orderService.cancelOrder(orderNumber);
