1e2a9b8ba7b4fb1330ba9fc1bc18571a4bfeb5ec
Vikas Dangi <mvikasdangi@gmail.com>
2025-05-03 01:16:25 +0530

    docs: added swagger documentation

---
+++ nexgen-server/cart-service/cart-service/src/main/java/com/nexgen/cart_service/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.nexgen.cart_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("NexGen Cart API")
+                .version("1.0.0")
+                .description("Documentation for NexGen Cart Microservice"));
+    }
+}
--- nexgen-server/cart-service/cart-service/src/main/java/com/nexgen/cart_service/controller/CartController.java
+++ nexgen-server/cart-service/cart-service/src/main/java/com/nexgen/cart_service/controller/CartController.java
@@ -2,6 +2,8 @@ package com.nexgen.cart_service.controller;
 
 import com.nexgen.cart_service.dto.*;
 import com.nexgen.cart_service.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +12,19 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/cart")
+@Tag(name = "Cart Controller", description = "Manage customer cart")
 @RequiredArgsConstructor
 public class CartController {
 
     private final CartService cartService;
 
+    @Operation(summary = "Get current items in the cart")
     @GetMapping("/{userId}")
     public ResponseEntity<List<CartItemResponse>> getCartByUser(@PathVariable String userId) {
         return ResponseEntity.ok(cartService.getCartByUser(userId));
     }
 
+    @Operation(summary = "Add an item to the cart")
     @PostMapping("/{userId}/add")
     public ResponseEntity<CartItemResponse> addItemToCart(
             @PathVariable String userId,
@@ -28,6 +33,7 @@ public class CartController {
         return ResponseEntity.ok(cartService.addItemToCart(userId, itemRequest));
     }
 
+    @Operation(summary = "Remove items from the cart")
     @DeleteMapping("/{userId}/remove/{productId}/quantity/{quantity}")
     public ResponseEntity<CartItemResponse> removeItemQuantity(
             @PathVariable String userId,
@@ -40,12 +46,14 @@ public class CartController {
         return ResponseEntity.ok(cartService.removeItemQuantity(userId, productId, quantity));
     }
 
+    @Operation(summary = "Clear the cart")
     @DeleteMapping("/{userId}/clear")
     public ResponseEntity<Void> clearCart(@PathVariable String userId) {
         cartService.clearCart(userId);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Pay and Checkout")
     @PostMapping("/checkout")
     public ResponseEntity<CheckoutResponse> checkout(@RequestBody CheckoutRequest request) {
         return ResponseEntity.ok(cartService.checkout(request));
